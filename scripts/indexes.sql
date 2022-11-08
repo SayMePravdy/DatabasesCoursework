6b359711691d3fea6e7cc79dddbc75d51a2b5e2c
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2022-11-08 20:27:37 +0300

    Update indexes.sql

--- scripts/indexes.sql
+++ scripts/indexes.sql
@@ -6,6 +6,9 @@ create index if not exists product_name_index
 
 create index if not exists product_partner_index
     on t_product(partner_id);
+    
+create index if not exists partner_review_index
+    on t_review(partner_id);
 
 create index if not exists user_card_index
     on t_loyalty_card(user_id);
