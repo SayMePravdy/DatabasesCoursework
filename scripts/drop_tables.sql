1101009c1a6984eea23ae343ed75a343b8e0c823
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2022-11-08 21:50:32 +0300

    Create drop_tables.sql

---
+++ scripts/drop_tables.sql
@@ -0,0 +1,24 @@
+drop table t_loyalty_card;
+
+drop table t_review;
+
+drop table t_order;
+
+drop table t_offer;
+
+drop table t_product;
+
+drop table t_order_status;
+
+drop table t_user_bank_card;
+
+drop table t_user;
+
+drop table t_partner;
+
+drop table t_photo;
+
+drop table t_role;
+
+drop table t_bank_card;
+
