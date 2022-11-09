7d76b2f55852781494ff92dbed48752736d0b643
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2022-11-09 12:24:57 +0300

    Update indexes.sql

--- scripts/indexes.sql
+++ scripts/indexes.sql
@@ -1,16 +1,16 @@
 create index if not exists user_order_index
-    on t_order(user_id);
+    on using hash t_order(user_id);
 
 create index if not exists product_name_index
     on t_product(name);
 
 create index if not exists product_partner_index
-    on t_product(partner_id);
+    on using hash t_product(partner_id);
     
 create index if not exists partner_review_index
-    on t_review(partner_id);
+    on using hash t_review(partner_id);
 
 create index if not exists user_card_index
-    on t_loyalty_card(user_id);
+    on using hash t_loyalty_card(user_id);
 
 --and all indexes on pk and unique fields
