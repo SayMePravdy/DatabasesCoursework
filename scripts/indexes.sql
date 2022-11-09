c1ab5feb5c5c5d3e1755e449910eea7648157b8c
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2022-11-09 12:34:46 +0300

    Update indexes.sql

--- scripts/indexes.sql
+++ scripts/indexes.sql
@@ -1,14 +1,14 @@
 create index if not exists user_order_index
-    on using hash t_order(user_id);
+    on t_order using hash (user_id);
 
 create index if not exists product_name_index
     on t_product(name);
 
 create index if not exists product_partner_index
-    on using hash t_product(partner_id);
+    on t_product using hash (partner_id);
     
 create index if not exists partner_review_index
-    on using hash t_review(partner_id);
+    on t_review using hash (partner_id);
 
 create index if not exists user_card_index
-    on using hash t_loyalty_card(user_id);
+    on t_loyalty_card using hash (user_id);
